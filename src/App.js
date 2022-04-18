7794571a9421b45ef1481a9e2f803f7a23250ed0
LucasKeylon <98484473+LucasKeylon@users.noreply.github.com>
2022-04-18 16:25:08 -0700

    Changes
    Added manual and excel <a> code and changed CSS file to accomodate.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome!
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          className="Excel-link"
+          href="https://docs.google.com/spreadsheets/d/1_RyT4Af2-h3I4QhH54W8xKC2mMbebXPHp1qDH9N4BL8/edit?usp=sharing"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Test q2\
+          Excel Data
+        </a>
+        <a
+          className="Manual-link"
+          href="https://docs.google.com/document/d/1B01L0DdIiTkL5GGSegKxEHFT2ej8w_uey8H95bxRleA/edit?usp=sharing"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          User Manual
         </a>
       </header>
     </div>
