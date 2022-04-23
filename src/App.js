f90199faa4d469007db7dd74ef047043023f2c92
LucasKeylon <98484473+LucasKeylon@users.noreply.github.com>
2022-04-23 11:58:38 -0700

    img txt change

--- src/App.js
+++ src/App.js
@@ -8,25 +8,28 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={img2} className="App-logo" alt="logo" />
-        <p>
-          Welcome!
+        <p className="greeting">
+          Welcome! 
+          <span>Please choose one of the below options.</span>
         </p>
-        <a
-          className="Excel-link"
-          href="https://docs.google.com/spreadsheets/d/1_RyT4Af2-h3I4QhH54W8xKC2mMbebXPHp1qDH9N4BL8/edit?usp=sharing"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Excel Data
-        </a>
-        <a
-          className="Manual-link"
-          href="https://docs.google.com/document/d/1B01L0DdIiTkL5GGSegKxEHFT2ej8w_uey8H95bxRleA/edit?usp=sharing"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          User Manual
-        </a>
+        <ul class="selections">
+          <a
+            className="nav-link"
+            href="https://docs.google.com/spreadsheets/d/1_RyT4Af2-h3I4QhH54W8xKC2mMbebXPHp1qDH9N4BL8/edit?usp=sharing"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Excel Data
+          </a>
+          <a
+            className="nav-link"
+            href="https://docs.google.com/document/d/1B01L0DdIiTkL5GGSegKxEHFT2ej8w_uey8H95bxRleA/edit?usp=sharing"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            User Manual
+          </a>
+        </ul>
       </header>
     </div>
   );
